166b7d0aeb62a13de67a37b06a2a381d9deac690
Sofia Vega <sofia.vega18@gmail.com>
2022-05-02 16:03:18 -0500

    debugear gramatica

--- parser.py
+++ parser.py
@@ -2,13 +2,13 @@ from lark import Lark
 
 
 parser = Lark(open("grammar", 'r').read())
-#goodInput = open("testcase_good.txt", 'r').read()
-#badInput = open("testcase_bad.txt", 'r').read()
+goodInput = open("testcase_facil.txt", 'r').read()
+badInput = open("testcase_good.txt", 'r').read()
 
-#print("------ Good Input --------")
+print("------ Good Input --------")
 
-#result = duck_parser.parse(goodInput)
-#print(result)
-#print("------ Bad Input --------")
-#result = duck_parser.parse(badInput)
-#print(result)
+result = parser.parse(goodInput)
+print(result)
+print("------ Input Dificil --------")
+result = parser.parse(badInput)
+print(result)
