1c250d417f16fd48804681eddbd3d69da23b687a
nadia.co <ngarciaorozco@outlook.com>
2022-05-03 14:00:42 -0500

    Directprio de procedimientos-ish?

--- birdhouse.py
+++ birdhouse.py
@@ -12,6 +12,7 @@ class VariableClass:
     - typeVar : string ->  tipo de la variable (numero, enunciado, bool, arreglo)
     - valueVar : [numero, bool, enunciado, arreglo] -> valor de la variable de acuerdo a su tipo
     - scopeVar : string -> scope de la variable
+    - addressVar : int -> direccion de memoria virtual
     '''
 
     def __init__(self, nameVar, typeVar, valueVar, scopeVar, addressVar):
@@ -39,3 +40,44 @@ class VariableTable:
         for item in self.tablaVar.items():
             print(item)
             item[1].printvar()
+
+
+# Clase Funcion para la creación de funciones y sus atributos
+class FunctionClass:
+    '''
+    Constructor de Function
+
+    Parámetros:
+    - nameFunc : string -> nombre de la funcion
+    - typeFunc : string ->  tipo de retorno de la funcion (numero, enunciado, bool, arreglo, void)
+    - paramsFunc : [] -> arreglo de parametros de tipo variable, o constante
+    - scopeFunc : string -> scope de la funcion
+    - addressFunc : int -> numero de cuadruplo donde inicia la funcion
+    '''
+
+    def __init__(self, nameFunc, typeFunc, paramsFunc, scopeFunc, addressFunc):
+        self.nameFunc = nameFunc
+        self.typeFunc = typeFunc
+        self.paramsFunc = paramsFunc
+        self.scopeFunc = scopeFunc
+        self.addressFunc = addressFunc
+        self.varsFunc = VariableTable()
+
+    def printfunc(self):
+        print("[nameFunc: {} typeFunc: {} paramsFunc: {} scopeFunc: {} addressFunc: {}]".format(
+            self.nameFunc, self.typeFunc, self.paramsFunc, self.scopeFunc, self.addressFunc))
+
+
+# Directorio de procedimientos
+class ProcDirectory:
+    def __init__(self):
+        self.procDirectory = {}
+
+    def addFunc(self, funcObject):
+        self.procDirectory[funcObject.nameFunc] = funcObject
+        print("Function added")
+
+    def printTable(self):
+        for item in self.procDirectory.items():
+            print(item)
+            item[1].printfunc()
