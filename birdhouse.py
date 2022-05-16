03b22b865d0512e7eb3e35411a155fdd94f2ccdf
Sofia Vega <sofia.vega18@gmail.com>
2022-05-16 13:53:29 -0500

    agregar algunos puntos neuralgicos

--- birdhouse.py
+++ birdhouse.py
@@ -5,6 +5,35 @@
 # Clase Variable para la creación de variables y sus atributos
 from lark import Visitor
 
+id_Asignar = ""
+pilaO = []
+pOper = []
+pSaltos = []
+
+
+
+# Tabla de Variables
+class VariableTable:
+    def __init__(self):
+        self.tablaVar = {}
+
+    def addVar(self, varObject):
+        self.tablaVar[varObject.nameVar] = varObject
+        print("Variable added")
+
+    def asignar(self, name, new_value):
+        self.tablaVar[name]
+
+    def printTable(self):
+        for item in self.tablaVar.items():
+            print(item)
+            item[1].printvar()
+
+
+#pilaO y poper
+tabla_variables = VariableTable()
+
+
 
 class VariableClass(Visitor):
     '''
@@ -17,32 +46,71 @@ class VariableClass(Visitor):
     - scopeVar : string -> scope de la variable
     - addressVar : int -> direccion de memoria virtual
     '''
-
+    '''
     def __init__(self, nameVar, typeVar, valueVar, scopeVar, addressVar):
         self.nameVar = nameVar
         self.typeVar = typeVar
         self.valueVar = valueVar
         self.scopeVar = scopeVar
         self.addressVar = addressVar
+        '''
 
     def printvar(self):
-        print("[nameVar: {} typeVar: {} valueVar: {} scopeVar: {} addressVar: {}]".format(
-            self.nameVar, self.typeVar, self.valueVar, self.scopeVar, self.addressVar))
+        print("[nameVar: {} typeVar: {} valueVar: {} scopeVar: {} addressVar: {}]".format(self.nameVar, self.typeVar, self.valueVar, self.scopeVar, self.addressVar))
+    def np_hola(self, tree):
+        print("hola")
+
+    def np_asignacion_1(self, tree):
+        #revisar si existe en la tabla de variables
+        id = tree.children[0].value
+        id_Asignar = id
+        if id in tabla_variables.tablaVar.keys():
+            print("todo bien")
+
+        else:
+            print("ERROR")
+
+        print("np 1")
+    def np_asignacion_2(self, tree):
+        #actualizar el valor
+        print("np 2")
+        print(tree.children[0].value)
+        #tabla_variables.tablaVar[id_Asignar]. tree.children[0.value]
+    '''
+    def asignacion(self, tree):
+        print("ASIGNACION")
+        print(tree)
+        print(tree.children[0].value)
+        print(tree.children[0].line)
+        print(tree.children[0])
+        #print(tree.children[0])
+    '''
+    
+    def idd(self, tree):
+        # 1 PilaO.Push(id.name) and PTypes.Push(id.type)
+        print("arbol en id")
+        print(tree)
+        print(tree.children)
+        miid = tree.children[1].value
+        print("mi id")
+        print(miid)
+        pilaO.append(miid)
+    
+    def f(self, tree):
+        #2.- POper.Push(* or /)
+        print("hijos de f")
+        print(tree.children)
+        signo = tree.children[0].value
+        pOper.append(signo)
+
+    def e(self, tree):
+        # 3.- POper.Push(+ or - )
+        print("hijos de e")
+        print(tree.children)
+        signo = tree.children[0].value
+        pOper.append(signo)
 
 
-# Tabla de Variables
-class VariableTable:
-    def __init__(self):
-        self.tablaVar = {}
-
-    def addVar(self, varObject):
-        self.tablaVar[varObject.nameVar] = varObject
-        print("Variable added")
-
-    def printTable(self):
-        for item in self.tablaVar.items():
-            print(item)
-            item[1].printvar()
 
 
 # Clase Funcion para la creación de funciones y sus atributos
@@ -70,7 +138,7 @@ class FunctionClass:
         print("[nameFunc: {} typeFunc: {} paramsFunc: {} scopeFunc: {} addressFunc: {}]".format(
             self.nameFunc, self.typeFunc, self.paramsFunc, self.scopeFunc, self.addressFunc))
 
-
+# x = 5
 # Directorio de procedimientos
 class ProcDirectory:
     def __init__(self):
--- parser.py
+++ parser.py
@@ -1,14 +1,24 @@
 from lark import Lark
+from lark import Visitor
+from birdhouse import *
 
 
 parser = Lark(open("grammar", 'r').read())
-goodInput = open("testcase_facil.txt", 'r').read()
+goodInput = open("testcase_aritmetica.txt", 'r').read()
 badInput = open("testcase_good.txt", 'r').read()
 
 print("------ Good Input --------")
 
 result = parser.parse(goodInput)
-print(result)
+print(result.pretty())
+for x in result.iter_subtrees_topdown():
+    print(x.data)
 print("------ Input Dificil --------")
-result = parser.parse(badInput)
-print(result)
+#result = parser.parse(badInput)
+#print(result.pretty())
+
+VariableClass().visit_topdown(result)
+print("pila o")
+print(pilaO)
+print("pila oper")
+print(pOper)
