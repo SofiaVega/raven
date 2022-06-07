8c2f8684c4421a18062d49bae39c58551e291774
Sofia Vega <sofia.vega18@gmail.com>
2022-06-06 22:47:27 -0500

    funciona factorial iterativo

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from raven import *
 
 
 parser = Lark(open("grammar", 'r').read())
-goodInput = open("testcases/testcase_bool.txt", 'r').read()
+goodInput = open("testcases/testcase_fact_linear.txt", 'r').read()
 badInput = open("testcases/testcase_good.txt", 'r').read()
 
 # print("------ Good Input --------")
