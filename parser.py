0b4830042994a73962910219810034a84bb0f9f6
Sofia Vega <sofia.vega18@gmail.com>
2022-06-06 22:55:56 -0500

    funciona fibonacci iterativo

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from raven import *
 
 
 parser = Lark(open("grammar", 'r').read())
-goodInput = open("testcases/testcase_fact_linear.txt", 'r').read()
+goodInput = open("testcases/testcase_fib_iter.txt", 'r').read()
 badInput = open("testcases/testcase_good.txt", 'r').read()
 
 # print("------ Good Input --------")
