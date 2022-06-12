4dfc9285a00cfb64f8f0bafd87939757a0e5e3ef
nadia.co <ngarciaorozco@outlook.com>
2022-06-11 21:13:12 -0500

    INTERFAZ TESTCASES

--- main.py
+++ main.py
@@ -7,11 +7,29 @@ testcases = ['testcase_oper_arreglos.txt',
              'testcase_fact_linear.txt',
              'testcase_funciones_fact.txt',
              'testcase_fib_iter.txt',
-             'testcase_find.txt']
+             'testcase_find.txt',
+             'testcase_opciones_2.txt']
 
 parser = Lark(open("grammar", 'r').read())
-input = open("testcases/testcase_fact_linear.txt", 'r').read()
 
-result = parser.parse(input)
-PuntosNeuralgicos().visit_topdown(result)
-maquinaVirtual()
+
+def compileAndExecute(option):
+    input = open("testcases/" + option, 'r').read()
+    result = parser.parse(input)
+    PuntosNeuralgicos().visit_topdown(result)
+    maquinaVirtual()
+
+
+def main():
+    print("Bienvenid@ al entorno de prueba de Raven.")
+    print("Seleccione el caso de prueba que desea utilizar para la evaluación de este lenguaje, su compilador y su máquina virtual.")
+    terminal_menu = TerminalMenu(testcases)
+    choice_index = terminal_menu.show()
+
+    print("\n---------- RESULTADOS CASO DE PRUEBA " +
+          testcases[choice_index] + " ----------\n")
+    compileAndExecute(testcases[choice_index])
+
+
+if __name__ == "__main__":
+    main()
