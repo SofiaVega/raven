7eb3753af767968e27fdaa6b733f154facaeb20d
nadia.co <ngarciaorozco@outlook.com>
2022-06-11 21:00:12 -0500

    interfaz de choose testcase en main

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from lark import Visitor
 from birdhouse import *
 from raven import *
 
-# testcase_oper_arreglos.txt
-# testcase_fact_linear.txt
-# testcase_funciones_fact.txt
-# testcase_fib_iter.txt
-# testcase_find.txt
+testcases = ['testcase_oper_arreglos.txt',
+             'testcase_fact_linear.txt',
+             'testcase_funciones_fact.txt',
+             'testcase_fib_iter.txt',
+             'testcase_find.txt']
 
 parser = Lark(open("grammar", 'r').read())
 input = open("testcases/testcase_fact_linear.txt", 'r').read()
